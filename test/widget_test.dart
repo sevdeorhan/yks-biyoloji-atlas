6899866a17e2087bb37e82109dcdff1118661816
Ceyhun Kemancı <ceyhun@Ceyhun-MacBook-Air.local>
2026-03-09 02:16:55 +0300

    removed unwanted file

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-// This is a basic Flutter widget test.
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('App compiles smoke test', (WidgetTester tester) async {
-    // Tests are currently disabled as they require Supabase and Hive initialization
-    expect(true, isTrue);
-  });
-}
